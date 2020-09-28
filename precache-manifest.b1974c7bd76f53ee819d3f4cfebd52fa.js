cde40caec28c9a5a8fb8f9d4735ea689459a2b01
Adam Klin <adam.klin@assecobs.pl>
2020-09-28 18:39:27 +0200

    Updates

---
+++ precache-manifest.b1974c7bd76f53ee819d3f4cfebd52fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4de57f82864875077bb31cd6faaafc9e",
+    "url": "/crypto-rate/index.html"
+  },
+  {
+    "revision": "d657c540540ed82cbcd7",
+    "url": "/crypto-rate/static/css/main.461745a9.chunk.css"
+  },
+  {
+    "revision": "071a5e721a79a605953f",
+    "url": "/crypto-rate/static/js/2.78e5727c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/crypto-rate/static/js/2.78e5727c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d657c540540ed82cbcd7",
+    "url": "/crypto-rate/static/js/main.285f4550.chunk.js"
+  },
+  {
+    "revision": "3e51928de1b5d527c4ca",
+    "url": "/crypto-rate/static/js/runtime-main.1f88f3df.js"
+  }
+]);
